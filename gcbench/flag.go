a7460642bd8018aea12f6fe8533dfccdde08077c
Austin Clements <(none)>
2016-04-01 21:04:18 -0400

    gcbench: improve data size parser and formatter
    This makes the Bytes parser understand units both like MB and like
    MiB, with their conventional meanings. The printer now always uses the
    SI prefix and prints the value as a decimal number in its natural
    unit, rather than an integer. This leads to much nicer formatting,
    especially when showing several sizes over a range, and it's more
    consistent with how time.Duration prints seconds.

--- gcbench/flag.go
+++ gcbench/flag.go
@@ -14,23 +14,34 @@ type Bytes int
 var si = []string{"", "k", "M", "G", "T", "P", "E", "Z", "Y"}
 
 const (
-	B  Bytes = 1
-	KB       = 1 << 10
-	MB       = 1 << 20
-	GB       = 1 << 30
-	TB       = 1 << 40
-	PB       = 1 << 50
-	EB       = 1 << 60
-	ZB       = 1 << 70
-	YB       = 1 << 80
+	B Bytes = 1
+
+	KB = 1e3
+	MB = 1e6
+	GB = 1e9
+	TB = 1e12
+	PB = 1e15
+	EB = 1e18
+	ZB = 1e21
+	YB = 1e24
+
+	KiB = 1 << 10
+	MiB = 1 << 20
+	GiB = 1 << 30
+	TiB = 1 << 40
+	PiB = 1 << 50
+	EiB = 1 << 60
+	ZiB = 1 << 70
+	YiB = 1 << 80
 )
 
 func (b Bytes) String() string {
+	f := float64(b)
 	for i, s := range si {
-		if b%1024 != 0 || b == 0 || i == len(si)-1 {
-			return fmt.Sprintf("%d%sB", b, s)
+		if f < 1000 || i == len(si)-1 {
+			return fmt.Sprintf("%g%sB", f, s)
 		}
-		b /= 1024
+		f /= 1000
 	}
 	panic("not reached")
 }
@@ -40,18 +51,35 @@ func (b *Bytes) Set(s string) error {
 	var unit string
 	_, err := fmt.Sscanf(s, "%g%s", &num, &unit)
 	if err == nil {
+		// Try SI prefixes first.
+		onum := num
 		for _, s := range si {
 			if unit == s+"B" {
+				*b = Bytes(num)
+				return nil
+			}
+			num *= 1000
+		}
+		// Try binary prefixes.
+		num = onum
+		for _, s := range si {
+			if unit == s+"iB" {
 				*b = Bytes(num)
 				return nil
 			}
 			num *= 1024
 		}
 	}
-	return fmt.Errorf("expected <num><prefix>B")
+	return fmt.Errorf("expected <num><SI or binary prefix>B")
 }
 
 func FlagBytes(name string, value Bytes, usage string) *Bytes {
 	flag.Var(&value, name, usage)
 	return &value
+}
+
+func ParseBytes(s string) (Bytes, error) {
+	var b Bytes
+	err := b.Set(s)
+	return b, err
 }
