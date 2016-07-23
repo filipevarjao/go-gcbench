f3c0aece60331e14062402abeea23b5ac2466c42
Austin Clements <(none)>
2016-07-23 11:22:54 -0400

    gcbench: record whole latency histogram

--- gcbench/lat.go
+++ gcbench/lat.go
@@ -5,14 +5,22 @@
 package gcbench
 
 import (
+	"fmt"
+	"io"
+	"math"
 	"sync/atomic"
 	"time"
 )
 
+// LatencyDist records a distribution of latencies in exponential
+// buckets between 1 ns and 1 sec.
 type LatencyDist struct {
-	N   int64
-	Max time.Duration
+	N       int64
+	Max     time.Duration
+	Buckets [100]int64
 }
+
+const latencyMax = time.Second
 
 func (d *LatencyDist) Start() *LatencyTracker {
 	return &LatencyTracker{dist: d, last: time.Now()}
@@ -20,12 +28,64 @@ func (d *LatencyDist) Start() *LatencyTracker {
 
 func (d *LatencyDist) add(t time.Duration) {
 	atomic.AddInt64(&d.N, 1)
+
 	max := time.Duration(atomic.LoadInt64((*int64)(&d.Max)))
 	for t > max {
 		if atomic.CompareAndSwapInt64((*int64)(&d.Max), int64(max), int64(t)) {
 			break
 		}
 		max = time.Duration(atomic.LoadInt64((*int64)(&d.Max)))
+	}
+
+	b := d.ToBucket(t)
+	atomic.AddInt64(&d.Buckets[b], 1)
+}
+
+var latencyBase = math.Log(float64(latencyMax))
+
+func (d *LatencyDist) ToBucket(t time.Duration) int {
+	if t < 1 {
+		return 0
+	}
+	// Log base latencyMax.
+	b := int(float64(len(d.Buckets)) * math.Log(float64(t)) / latencyBase)
+	if b < 0 {
+		return 0
+	}
+	if b >= len(d.Buckets) {
+		return len(d.Buckets) - 1
+	}
+	return b
+}
+
+func (d *LatencyDist) FromBucket(b int) (lo, hi time.Duration) {
+	// b = n * log_1s(t) => t = 1s ^ (b / n)
+	if b == 0 {
+		lo = 0
+	} else {
+		lo = time.Duration(math.Pow(float64(latencyMax), float64(b)/float64(len(d.Buckets))))
+	}
+	hi = time.Duration(math.Pow(float64(latencyMax), float64(b+1)/float64(len(d.Buckets))))
+	return
+}
+
+func (d *LatencyDist) Fprint(w io.Writer) {
+	minb, maxb := -1, 1
+	for i, count := range d.Buckets {
+		if count > 0 {
+			if minb == -1 {
+				minb = i
+			}
+			maxb = i
+		}
+	}
+	if minb == -1 {
+		fmt.Fprintf(w, "no samples\n")
+		return
+	}
+	for b := minb; b <= maxb; b++ {
+		lo, hi := d.FromBucket(b)
+		fmt.Fprintf(w, "[%12s,%12s) %d\n", lo, hi, d.Buckets[b])
 	}
 }
 
