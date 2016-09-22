d3687a4b9aae626bacbdddde3e08b605e5b77075
Austin Clements <austin@google.com>
2016-09-21 21:54:48 -0400

    gcbench/progs: stack size control for IdleGs

--- gcbench/progs/idlegs.go
+++ gcbench/progs/idlegs.go
@@ -14,30 +14,43 @@ import (
 )
 
 const (
+	ptrSize = 4 << (^uintptr(0) >> 63)
+
 	ballastSize   = 100 << 20
-	garbageSize   = 10 << 20
-	garbagePeriod = 100 * time.Millisecond
+	garbagePerSec = 100 << 20
 )
-
-var (
-	ballast []byte
-	garbage []byte
-)
-
-func churn() {
-	ballast = make([]byte, ballastSize)
-
-	for {
-		time.Sleep(garbagePeriod)
-		garbage = make([]byte, garbageSize)
-	}
-}
 
 var (
 	flagDuration = flag.Duration("benchtime", 10*time.Second, "steady state duration")
 	// 5e5 Gs uses about 1.5GB of memory.
-	flagGs = flag.Int("idle-gs", 5e5, "start `n` idle goroutines")
+	flagGs        = flag.Int("idle-gs", 5e5, "start `n` idle goroutines")
+	flagStackSize = gcbench.FlagBytes("stack-size", 0, "stack size")
 )
+
+const frameSize = 512
+
+func withStack(size gcbench.Bytes, f func()) {
+	// TODO: Make this a gcbench package util?
+	if size < frameSize {
+		f()
+	} else {
+		withStack1(size, f)
+	}
+}
+
+func withStack1(size gcbench.Bytes, f func()) uintptr {
+	// Use frameSize bytes of stack frame.
+	var thing [(frameSize - 4*ptrSize) / ptrSize / 2]struct {
+		s uintptr
+		p *byte
+	}
+	if size <= frameSize {
+		f()
+	} else {
+		withStack1(size-frameSize, f)
+	}
+	return thing[0].s
+}
 
 func main() {
 	flag.Parse()
@@ -51,10 +64,21 @@ func main() {
 
 func benchMain() {
 	for i := 0; i < *flagGs; i++ {
-		go func() { select {} }()
+		if *flagStackSize == 0 {
+			go func() { select {} }()
+		} else {
+			go func() {
+				withStack(*flagStackSize, func() {
+					select {}
+				})
+			}()
+		}
 	}
 
-	go churn()
+	(&gcbench.Churner{
+		BallastBytes: ballastSize,
+		BytesPerSec:  garbagePerSec,
+	}).Start()
 
 	time.AfterFunc(*flagDuration, func() { os.Exit(0) })
 	select {}
